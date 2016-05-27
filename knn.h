a9bd0ad8fd5b9d7627c2258fa7c1fb09556a9441
Shuyang Shi <shuyang790@gmail.com>
2016-05-27 17:33:02 +0800

    tidy knn.h

--- knn.h
+++ knn.h
@@ -64,41 +64,7 @@ private:
      *  used to classify a single item, using K-Nearest-Neighbors
      */
     D classify(
-            const Data <T> &data) {
-        priority_queue < pair<int, D> > q;
-        while (!(q.empty()))
-            q.pop();
-        for (auto i=labeledData.begin(); i!=labeledData.end(); i++) {
-            pair <int, D> p = make_pair(DataDist(i->val, data.val), i->label);
-            if (q.size() < K)
-                q.push(p);
-            else if (q.top().first > p.first) {
-                q.pop();
-                q.push(p);
-            }
-        }
-
-        vector <D> vec;
-        for (; !q.empty(); q.pop())
-            vec.push_back(q.top().second);
-        sort(vec.begin(), vec.end(), LabelCmp);
-
-        int maxCount = 0;
-        int curCount = 0;
-        D label = *vec.begin();
-        for (auto i = 0; i < vec.size(); i++){
-            if (!i || vec[i] == vec[i-1])
-                curCount++;
-            else
-                curCount = 1;
-            if (curCount > maxCount){
-                maxCount = curCount;
-                label = vec[i];
-            }
-        }
-
-        return label;
-    }
+            const Data <T> &data);
 
 public:
 
@@ -112,12 +78,7 @@ public:
     KNNClassifier(
             int _K,
             Data2int _DataDist,
-            Label2int _LabelCmp) {
-        K = _K;
-        DataDist = _DataDist;
-        LabelCmp = _LabelCmp;
-        trained = 0;
-    }
+            Label2int _LabelCmp);
 
     /*
      * KNNClassifier train
@@ -125,11 +86,7 @@ public:
      *      and set 'trained' flag to True
      */
     void train(
-            const vector < DataWithLabel <T, D> > & dataWithLabel) {
-        for (auto i=dataWithLabel.begin(); i!=dataWithLabel.end(); i++)
-            labeledData.push_back(*i);
-        trained = 1;
-    }
+            const vector < DataWithLabel <T, D> > & dataWithLabel);
 
     /*
      * Classify a list of items
@@ -138,40 +95,116 @@ public:
      */
     void classify(
             const vector < Data <T> > &data,
-            vector < Data <D> > &labels) {
-        if (!trained)
-            cerr << "Classifier used to classify before training!" << endl;
+            vector < Data <D> > &labels);
 
-        labels.clear();
-        for (auto i = data.begin(); i != data.end(); ++i) {
-            labels.push_back(classify(*i));
-        }
-    }
-
+    /*
+     * Test using training data and test data
+     *  This includes:
+     *      - train (or at least move data to classifier)
+     *      - classify
+     *      - judge and report and correct ratio (to stderr)
+     */
     void test(
             const vector < DataWithLabel<T, D> > &train,
             const vector < Data<T> > &testData,
-            const vector < Data<D> > &testLabels){
-
-        vector < Data<D> > testResults;
-        this->train(train);
-        cerr << "[INFO] training finished." << endl;
-
-        classify(testData, testResults);
-        cerr << "[INFO] classification finished. " << endl;
-
-        int tot = (int) testResults.size();
-        int correct = 0;
-        for (int i=0; i<tot; i++)
-        if (testResults[i].val == testLabels[i].val)
-        correct++;
-
-        cerr << "[RESULT] " << correct << " of " << tot << " correct: " << correct * 100.0 / tot << "%" << endl;
-
-    }
+            const vector < Data<D> > &testLabels);
 };
 
+/****************************************************************************/
+
+template <typename T, typename D>
+D KNNClassifier <T, D>::classify(
+        const Data <T> &data) {
+    priority_queue < pair<int, D> > q;
+    while (!(q.empty()))
+        q.pop();
+    for (auto i=labeledData.begin(); i!=labeledData.end(); i++) {
+        pair <int, D> p = make_pair(DataDist(i->val, data.val), i->label);
+        if (q.size() < K)
+            q.push(p);
+        else if (q.top().first > p.first) {
+            q.pop();
+            q.push(p);
+        }
+    }
+
+    vector <D> vec;
+    for (; !q.empty(); q.pop())
+        vec.push_back(q.top().second);
+    sort(vec.begin(), vec.end(), LabelCmp);
+
+    int maxCount = 0;
+    int curCount = 0;
+    D label = *vec.begin();
+    for (auto i = 0; i < vec.size(); i++){
+        if (!i || vec[i] == vec[i-1])
+            curCount++;
+        else
+            curCount = 1;
+        if (curCount > maxCount){
+            maxCount = curCount;
+            label = vec[i];
+        }
+    }
+
+    return label;
+}
 
 
+template <typename T, typename D>
+KNNClassifier<T, D>::KNNClassifier(
+        int _K,
+        Data2int _DataDist,
+        Label2int _LabelCmp) {
+    K = _K;
+    DataDist = _DataDist;
+    LabelCmp = _LabelCmp;
+    trained = 0;
+}
+
+template <typename T, typename D>
+void KNNClassifier<T, D>::train(
+        const vector < DataWithLabel <T, D> > & dataWithLabel) {
+    for (auto i=dataWithLabel.begin(); i!=dataWithLabel.end(); i++)
+        labeledData.push_back(*i);
+    trained = 1;
+}
+
+template <typename T, typename D>
+void KNNClassifier<T, D>::classify(
+        const vector < Data <T> > &data,
+        vector < Data <D> > &labels) {
+    if (!trained)
+        cerr << "Classifier used to classify before training!" << endl;
+
+    labels.clear();
+    for (auto i = data.begin(); i != data.end(); ++i) {
+        labels.push_back(classify(*i));
+    }
+}
+
+template <typename T, typename D>
+void KNNClassifier<T, D>::test(
+        const vector < DataWithLabel<T, D> > &train,
+        const vector < Data<T> > &testData,
+        const vector < Data<D> > &testLabels){
+
+    vector < Data<D> > testResults;
+    this->train(train);
+    cerr << "[INFO] training finished." << endl;
+
+    classify(testData, testResults);
+    cerr << "[INFO] classification finished. " << endl;
+
+    int tot = (int) testResults.size();
+    int correct = 0;
+    for (int i=0; i<tot; i++)
+        if (testResults[i].val == testLabels[i].val)
+            correct++;
+
+    cerr << "[RESULT] " << correct << " of " << tot << " correct: "
+    << correct * 100.0 / tot << "%" << endl;
+
+}
 
 #endif //HANDWRITINGDIGITS_KNN_H
